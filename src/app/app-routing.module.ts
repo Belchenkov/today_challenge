815be00eac9002df7d74b04003302c65d4b29808
Aleksey Belchenkov <u608110@gmail.com>
2020-05-01 12:31:23 +0300

    Implementing Page Navigation

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,16 @@ import { NativeScriptRouterModule } from "nativescript-angular/router";
 import { Routes } from "@angular/router";
 
 import { AuthComponent } from "~/app/auth/auth.component";
+import { TodayComponent } from "~/app/challenges/today/today.component";
 
 const routes: Routes = [
     {
         path: '',
         component: AuthComponent
+    },
+    {
+        path: 'today',
+        component: TodayComponent
     }
 ];
 
--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from "@angular/router";
 
 @Component({
   selector: 'ns-auth',
@@ -8,9 +9,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AuthComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+      private router: Router
+  ) { }
 
   ngOnInit() {
+  }
+
+  onSignIn() {
+    this.router.navigate(['/today']);
   }
 
 }
