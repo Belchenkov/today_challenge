6e836ef712406b1d83faaeeda85be5718a7d209f
Aleksey Belchenkov <u608110@gmail.com>
2020-05-06 14:44:00 +0300

    Adding a Sidedrawer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule, NO_ERRORS_SCHEMA } from "@angular/core";
 import { NativeScriptModule } from "nativescript-angular/nativescript.module";
 import { FormsModule } from "@angular/forms";
 import { NativeScriptFormsModule } from "nativescript-angular";
+import { NativeScriptUISideDrawerModule } from 'nativescript-ui-sidedrawer/angular/side-drawer-directives';
 
 import { AppComponent } from "./app.component";
 import { CurrentChallengeComponent } from "~/app/challenges/current-challenge/current-challenge.component";
@@ -20,6 +21,7 @@ import { ChallengeTabsComponent } from './challenges/challenge-tabs/challenge-ta
         NativeScriptModule,
         FormsModule,
         NativeScriptFormsModule,
+        NativeScriptUISideDrawerModule,
         AppRoutingModule
     ],
     declarations: [
